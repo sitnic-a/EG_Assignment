46b9dd6ae07e195248a20cb5d7ea627e3b1d50e3
sitnic-a <admir.hhs@gmail.com>
2023-11-28 19:14:18 +0100

    MVC app

---
+++ ExordiumGames.MVC/Program.cs
@@ -0,0 +1,63 @@
+using ExordiumGames.MVC.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+/* 1) Seed roles
+
+   2) Seed one user - Admin
+
+   3) Add additional tables : Retailers, Categories and Items
+
+   4) - User controller
+            - Read and write Retailer and Category filters set in Unity application
+
+   5) - Employee
+            - Add/remove/update (CRUD) of data:
+            - Retailer
+            - Category
+            - Items
+            - Everything else the User role scan
+   ) - User
+            - Read and write Retailer and Category filters set in Unity application
+*/
+
+
+app.Run();
