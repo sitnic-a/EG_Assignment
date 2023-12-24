a1ddd878fffebe89e84edf5012d967e5ad0e7e3b
sitnic-a <admir.hhs@gmail.com>
2023-12-24 13:26:23 +0100

    GetItems feature - Unity WIP

---
+++ ExordiumGames.MVC/Controllers/UnityController.cs
@@ -0,0 +1,26 @@
+﻿using ExordiumGames.MVC.Data.DbModels;
+using ExordiumGames.MVC.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExordiumGames.MVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnityController : ControllerBase
+    {
+        private readonly IEmployeeService<Category, Item, Retailer> _employeeService;
+
+        public UnityController(IEmployeeService<Category,Item,Retailer> employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet("items")]
+        public async Task<List<Item>> GetItemsUnityEndpoint()
+        {
+            var dbItems = await _employeeService.GetItems();
+            return dbItems;
+        }
+    }
+}
---
+++ ExordiumGames.Unity/Assets/Script/UserAccount.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserAccount : MonoBehaviour
+{
+    public void CallGetItems()
+    {
+        Debug.Log("Items called");
+    }
+}
